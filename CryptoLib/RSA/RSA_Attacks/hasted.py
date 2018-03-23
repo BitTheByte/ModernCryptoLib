5b7e57e4fe89679d14baa2ca64cd482cb84f6b39
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2018-03-23 20:58:19 +0200

    Update hasted.py

--- CryptoLib/RSA/RSA_Attacks/hasted.py
+++ CryptoLib/RSA/RSA_Attacks/hasted.py
@@ -1,6 +1,6 @@
 from utilities import mulinv,powinv
 
-def hasted(n1,n2,n3,chipher_text_1,chipher_text_2,chipher_text_3):
+def hasted(n1,n2,n3,chipher_text_1,chipher_text_2,chipher_text_3,e):
     n = [n1,n2,n3]
     a = [chipher_text_1,chipher_text_2,chipher_text_3]
     sum = 0
@@ -8,4 +8,4 @@ def hasted(n1,n2,n3,chipher_text_1,chipher_text_2,chipher_text_3):
     for n_i, a_i in zip(n, a):
         p = prod / n_i
         sum += a_i * mulinv(p, n_i) * p
-    return powinv(sum % p)
+    return powinv(sum % p,e)
