de936f2868145d1bea3eda4b0dbe1718011364c5
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2018-04-20 11:15:37 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+        name = 'ModernCryptoLib',
+        version = "0.0.9",
+        description = 'Cryptography Library ',
+        license = "MIT",
+        author = "Ahmed Ezzat (BitTheByte)",
+        packages = find_packages(),
+        install_requires = ['pycrypto','requests','sym','cryptography','sympy'],
+    )
