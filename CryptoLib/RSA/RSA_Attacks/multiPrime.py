850a633506fa5dcd01e8bbd531ab92e22172c298
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2018-04-23 10:16:16 +0200

    Create multiPrime.py

---
+++ CryptoLib/RSA/RSA_Attacks/multiPrime.py
@@ -0,0 +1,21 @@
+from utilities import egcd,modinv
+
+def multiPrime(primes_array,n,e,c):
+    primes = []
+    ts = []
+    xs = []
+    ds = []
+    for i in range(len(primes)):
+    	ds.append(modinv(e, primes[i]-1))
+    m = primes[0]
+    for i in range(1, len(primes)):
+    	ts.append(modinv(m, primes[i]))
+    	m = m * primes[i]
+    for i in range(len(primes)):
+    	xs.append(pow((c%primes[i]), ds[i], primes[i]))
+    x = xs[0]
+    m = primes[0]
+    for i in range(1, len(primes)):
+    	x = x + m * ((xs[i] - x % primes[i]) * (ts[i-1] % primes[i]))
+    	m = m * primes[i]
+    return hex(x%n)[2:-1].decode("hex")
