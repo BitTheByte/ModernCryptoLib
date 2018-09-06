e8faa95a2fde94492c527a14b504d21f7455817a
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2018-09-06 17:23:44 +0200

    Update wiener.py

--- CryptoLib/RSA/RSA_Attacks/wiener.py
+++ CryptoLib/RSA/RSA_Attacks/wiener.py
@@ -1,29 +1,30 @@
-from utilities import convergents_from_contfrac
-from utilities import is_perfect_square
-from utilities import rational_to_contfrac
-from sympy.solvers import solve
-from sympy import Symbol
-import sys
+from utilities import SquareAndMultiply
+from utilities import powinv
+from utilities import DevContinuedFraction
+from utilities import DivergentsComputation
 
-def wiener(n,e):
-    d = None
-    p = None
-    q = None
-    sys.setrecursionlimit(100000)
-    frac = rational_to_contfrac(e, n)
-    convergents = convergents_from_contfrac(frac)
-    for (k,d) in convergents:
-        if k!=0 and (e*d-1)%k == 0:
-            phi = (e*d-1)//k
-            s = n - phi + 1
-            discr = s*s - 4*n
-            if(discr>=0):
-                t = is_perfect_square(discr)
-                if t!=-1 and (s+t)%2==0:
-                    x = Symbol('x')
-                    roots = solve(x**2 - s*x + n, x)
-                    if len(roots) == 2:
-                        p = roots[0]
-                        q = roots[1]
-                    break
-    return {'p':p,'q':q,'d':d}
+
+def __helper(N,E,c):
+    phi = (E*c[1]-1)//c[0]
+    a = 1
+    b = -(N-phi+1)
+    c = N
+    delta =b*b - 4*a*c
+    if delta > 0 :
+        x1 = (-b + powinv((b*b - 4*a*c), 2))/(2*a)
+        x2 = (-b - powinv((b*b - 4*a*c), 2))/(2*a)
+        if x1*x2 == N:
+        	return {'q':x1,'p':x2}
+        else:
+        	return {'q':None,'p':None}
+    else:
+        return {'q':None,'p':None}
+            
+def Wiener(N,E) :
+	testStr = 42 
+	phi = 0
+	C = SquareAndMultiply(testStr, E, N)
+	for c in DivergentsComputation(DevContinuedFraction(E, N)) :
+		if SquareAndMultiply(C, c[1], N) == testStr :
+		    return __helper(N,E,c)
+	return {'q':None,'p':None}
