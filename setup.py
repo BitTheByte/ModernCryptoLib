85dee8781d9202227fbf7aeefa4543b7879260ee
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2018-09-06 17:24:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'ModernCryptoLib',
-        version = "0.0.11",
+        version = "0.0.12",
         description = 'Cryptography Library ',
         license = "MIT",
         author = "Ahmed Ezzat (BitTheByte)",
